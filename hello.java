163820aab2bae5ad19d91b0fcdaad55b818d120e
Muthukumaran7676 <161341542+Muthukumaran7676@users.noreply.github.com>
2024-09-21 10:21:51 +0530

    test

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class hello{
+   public static void main(String args[]){
+
+      int score = 90;
+
+      if(score>35 && score<60)
+      {
+            System.out.print("video game");
+      }    
+      else if(score>=60 && score<90)
+      {
+         System.out.print("iphone");
+      }
+      else if (score>=90)
+      {
+         System.out.print("macbook pro");
+      }
+    }
+}
---
+++ q1.java
@@ -0,0 +1,13 @@
+class q1{
+    public static void main(String args[]);
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = a*b*c;
+        int e = a+b+c;
+
+        system.out.println(d/e);
+    }
+} 
