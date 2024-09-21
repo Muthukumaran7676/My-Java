c7a81bda814e210c38eb42bcbca07e2679a32c0d
Muthukumaran7676 <abikumaran001@gmail.com>
2024-09-21 15:11:41 +0530

    Update

---
+++ kumaran.java
@@ -0,0 +1,16 @@
+class kumaran
+{public static void main(String args[])
+    { int a;
+      float b;
+      char ch;
+      String name;
+      boolean val;
+      a=20;
+      b=22;
+      ch='b';
+      name="kumaran";
+      val=true;
+      System.out.println(a+"\n"+b+"\n"+ch);
+      System.out.println(name+"\n"+val);
+  }
+}
