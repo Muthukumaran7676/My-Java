0fea1e414fd901f85b0dfa0009a0afb3ae116c97
Muthukumaran7676 <abikumaran001@gmail.com>
2024-09-21 16:08:58 +0530

    Update

--- kumaran.java
+++ kumaran.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 class kumaran
 {public static void main(String args[])
-    { int a;
+   /* { int a;
       float b;
       char ch;
       String name;
@@ -12,5 +14,19 @@ class kumaran
       val=true;
       System.out.println(a+"\n"+b+"\n"+ch);
       System.out.println(name+"\n"+val);
+  }*/
+  { 
+    Scanner sc = new Scanner(System.in);
+        int age;
+        float fees;
+        char gender;
+        String name;
+        System.out.println("Enter the given details:");
+        age = sc.nextInt();
+        fees = sc.nextFloat();
+        gender = sc.next().charAt(0);
+        name = sc.nextLine();
+        name = sc.nextLine();
+        System.out.println("Name: "+name+"\n"+"Age: "+age+"\n"+"Gender: "+gender+"\n"+"Fees: "+fees);2525
   }
 }
