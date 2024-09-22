dd641f67ef2f6e61d401a560a283091983779901
Muthukumaran7676 <abikumaran001@gmail.com>
2024-09-22 10:07:40 +0530

    Update

--- kumaran.java
+++ kumaran.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class kumaran
-{public static void main(String args[])
+
    /* { int a;
       float b;
       char ch;
@@ -14,7 +14,7 @@ class kumaran
       val=true;
       System.out.println(a+"\n"+b+"\n"+ch);
       System.out.println(name+"\n"+val);
-  }*/
+  }
   { 
     Scanner sc = new Scanner(System.in);
         int age;
@@ -28,5 +28,18 @@ class kumaran
         name = sc.nextLine();
         name = sc.nextLine();
         System.out.println("Name: "+name+"\n"+"Age: "+age+"\n"+"Gender: "+gender+"\n"+"Fees: "+fees);2525
+  }*/
+  {
+      int a=10;
+      static int b= 20;      
+      void display()
+      {
+        a=20;
+        b=200;
+        System.out.println("a:"+a+"\n"+"b:"+b);
+      }
+      public static void main(String[]args){
+        kumaran k=new kumaran();
+       k.display();
+      }
   }
-}
