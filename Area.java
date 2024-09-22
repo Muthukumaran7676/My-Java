b1421c875777d2a7e6e3e91c27f4ff69e4b724df
Muthukumaran7676 <abikumaran001@gmail.com>
2024-09-22 15:47:59 +0530

    Update

--- Area.java
+++ Area.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Area
+/*public class Area
 {
   int Square(int a)
   {
@@ -28,3 +28,60 @@ public class Area
        System.out.println(r);
     }
 }
+public class Area{
+        public static void main(String[] args) {
+        /*String s = "hello all";
+        System.out.println("length:"+s.length());
+        s=s.trim();
+        System.out.println("length:"+s.length());           
+        System.out.println(s.indexOf('1'));
+        System.out.println(s.lastIndexOf('1'));
+        System.out.println(s.charAt(3));
+        System.out.println(s.toUpperCase());
+
+        String str="welcome";
+        String str1="weLCOME";
+        System.out.println(str.equals(str1));
+        System.out.println(str.equalsIgnoreCase(str1));
+        }
+
+}
+
+import java.util.*;
+class Length
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String name= sc.nextLine();
+        int count=0;
+        for(int i=0;i<=name.length();i++)
+        {
+            char x =name.charAt(i);
+             System.out.println(x);
+            count+=count;
+           
+        }
+     System.out.println(count);  
+    }
+}*/
+
+public class Area {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        int i,count=0;
+        char ch[]=s.toCharArray();
+        for(char c:ch)
+        {
+            count++;
+        }
+        System.out.println(count);
+        for(i=s.length()-1;i>=0;i--){
+            System.out.print(s.charAt(i));
+    }
+    }
+}
+
+ 
