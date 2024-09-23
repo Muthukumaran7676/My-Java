068fd2cb77662a069bab0322b82080c3baba4c3d
Muthukumaran7676 <abikumaran001@gmail.com>
2024-09-23 16:09:09 +0530

    Update

--- Oops.java
+++ Oops.java
@@ -24,14 +24,28 @@ class developer extends Employee
     }
     void display(){
         super.display();
-        System.out.println("proglan:"+proglan);
+        System.out.println("well in: "+proglan);
     }
 }
+class programmer extends Employee{
+    String programmer;
+    programmer(int empid,String name,double sal,String programmer){
+        super(empid, name, sal);
+        this.programmer=programmer;
+    }
+void display(){
+    super.display();
+    System.out.println("expert in: "+programmer);
+}
+}
+
 public class Oops {
     public static void main(String[] args) {
         Employee emp1=new Employee(101,"abi",100000);
         developer dev1=new developer(102,"prithivi",150000,"java");
+        programmer pro1=new programmer(103,"abishek", 200000, "python");
         emp1.display();
         dev1.display();
+        pro1.display();
     }
 }
